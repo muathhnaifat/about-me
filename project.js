b4da516370c369e1afc9e56ab4c73b826b324383
Muath <muath_hnaifat@yahoo.com>
2019-10-07 16:25:14 +0300

    new

---
+++ project.js
@@ -0,0 +1,72 @@
+'use srict'
+
+var love = prompt ('Do i love coding?');
+love = love.toLowerCase();
+
+if(love ==='yes' ||love=== 'y') {
+    alert ('of course I do') ;
+}
+else if (love === 'no'|| love ==='n'){
+alert (' You should know me more ');
+}
+else  {
+    alert ('You should answer with yes or no')
+
+}
+
+var car = prompt ('BMW is my favorite car');
+car = car.toLowerCase();
+
+if(car ==='yes' || car=== 'y') {
+    alert ('Of course BMW the best') ;
+}
+else if (car === 'no' || car=== 'n'){
+alert (' Ohh , don\'t tell me theres any car best than BMW ');
+}
+else  {
+    alert ('You should answer with yes or no')
+}
+
+
+var football = prompt ('Real Madrid My Dream Team');
+football = football.toLowerCase();
+
+if(football ==='yes' || football=== 'y') {
+    alert ('that\'s the Absolute Truth') ;
+}
+else if (football === 'no' || football=== 'n'){
+alert (' No No No !! That\'s the most important thing you should know about me ');
+}
+else  {
+    alert ('You should answer with yes or no')
+}
+
+
+
+var movies = prompt ('i love watch romance movies');
+movies = movies.toLowerCase();
+
+if(movies ==='yes' || movies=== 'y') {
+    alert (' I love action and drama , but I found romance movies boring ') ;
+}
+else if (movies === 'no' || movies=== 'n'){
+alert (' it seems you know my favorite type of movies ');
+}
+else  {
+    alert ('You should answer with yes or no')
+}
+
+
+
+var live = prompt (' i wish to travel and live in canada');
+live = live.toLowerCase();
+
+if(live ==='yes' || live=== 'y') {
+    alert (' canada is a wonderfull country , i love it  ') ;
+}
+else if (live === 'no' || live=== 'n'){
+alert (' you should try to know more about me ');
+}
+else  {
+    alert ('You should answer with yes or no')
+}
